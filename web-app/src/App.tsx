dda2a0c58eb89175069f4972a5add64c683954c6
rajmapro <rohith009srk@gmail.com>
2024-11-25 15:24:08 +0530

    first commit

---
+++ web-app/src/App.tsx
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import NxtWatchContext from "./context/NxtWatchContext";
+import Home from "./pages/Home";
+import Trending from "./pages/Trending";
+import Gaming from "./pages/Gaming";
+import VideoPlayer from "./pages/Videoplayer";
+import SavedVideo from "./pages/SavedVideos";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+interface stateData {
+  darkMode: boolean;
+  premiumDisplay: boolean;
+  savedVideosList: VideoDataType[];
+}
+interface VideoDataType {
+  id: string;
+  publishedAt: Date;
+  thumbnail: string;
+  title: string;
+  views: string;
+  name: string;
+  profileImg: string;
+  videoUrl: string;
+  subscribers: String;
+  description: string;
+}
+
+interface Contextvalue {
+  darkMode: boolean;
+  premiumDisplay: boolean;
+  darkModeFunc: () => void;
+  closePremium: () => void;
+  saveVideosFunc: (video: VideoDataType) => void;
+  savedVideosList: VideoDataType[];
+}
+
+class App extends Component<{}, stateData> {
+  state: stateData = {
+    darkMode: false,
+    premiumDisplay: true,
+    savedVideosList: [],
+  };
+
+  darkModeFunc = () => {
+    this.setState((prevState) => ({
+      darkMode: !prevState.darkMode,
+    }));
+    console.log("entered DarkMode");
+  };
+
+  closePremium = () => {
+    this.setState((prevState) => ({
+      premiumDisplay: !prevState.premiumDisplay,
+    }));
+  };
+
+  saveVideosFunc = (video: VideoDataType) => {
+    this.setState((prevState) => {
+      const isSaved = prevState.savedVideosList.find(
+        (savedVideo: VideoDataType) => savedVideo.id === video.id
+      );
+      if (isSaved) {
+        return {
+          savedVideosList: prevState.savedVideosList.filter(
+            (savedVideo: VideoDataType) => savedVideo.id !== video.id
+          ),
+        };
+      }
+      return {
+        savedVideosList: [...prevState.savedVideosList, video],
+      };
+    });
+  };
+
+  render(): React.ReactNode {
+    const contextvalue: Contextvalue = {
+      darkMode: this.state.darkMode,
+      darkModeFunc: this.darkModeFunc,
+      closePremium: this.closePremium,
+      saveVideosFunc: this.saveVideosFunc,
+      premiumDisplay: this.state.premiumDisplay,
+      savedVideosList: this.state.savedVideosList,
+    };
+    return (
+      <BrowserRouter>
+        <NxtWatchContext.Provider value={contextvalue}>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/trending" element={<Trending />} />
+              <Route path="/gaming" element={<Gaming />} />
+              <Route path="/videos/:id/" element={<VideoPlayer />} />
+              <Route path="/saved-videos" element={<SavedVideo />} />
+            </Route>
+          </Routes>
+        </NxtWatchContext.Provider>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
